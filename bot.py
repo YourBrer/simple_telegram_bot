c801dade5ffd91d4c8ccf316e64b96a8db514481
Your_Bro <pontiy.pilot@gmail.com>
2022-01-24 15:52:00 +0400

    Правки

--- bot.py
+++ bot.py
@@ -21,49 +21,50 @@ def send_welcome(message):
     bot.send_message(message.chat.id, help_message)
 
 
+@bot.message_handler(func=lambda message: message.chat.id not in config.user_ids_with_access)
+def some(message):
+    img = open('./who are you.jpg', 'rb')
+    bot.send_photo(message.chat.id, img)
+
+
 @bot.message_handler(content_types=['text'])
 def main(message):
     """
-    Основная функция взаимодействия с пользователем. Получает сообщение и на основе первого символа
+    Основная функция взаимодействия с пользователем. Получает сообщение и на основе первого символа -
     либо, если символ '!', разбирает сообщение на составляющие и сохраняет данные в БД,
     либо, если символ '?', ищет все вхождения в БД и выводит их в ответном сообщении.
     """
-    # т.к. изначально бот выполнял другие функции и был доступен большому количеству людей, чтобы не регистрировать
-    # новый токен, я решил сделать проверку id отправителя сообщений и отвечать только избранным пользователям,
-    # чьи id сохранил в файле конфига
-    if message.from_user.id in config.user_ids_with_access:
-        conn = sqlite3.connect('passwords.db')
-        cur = conn.cursor()
-        cur.execute("""CREATE TABLE IF NOT EXISTS passwords(
-           id INTEGER PRIMARY KEY AUTOINCREMENT,
-           site_name TEXT,
-           login TEXT,
-           password TEXT);
-        """)
+    conn = sqlite3.connect('passwords.db')
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS passwords(
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       site_name TEXT,
+       login TEXT,
+       password TEXT);
+    """)
+    conn.commit()
+    if message.text.strip()[0] == '!':
+        record = message.text[1:].strip().split(',')
+        site_name = record[0].strip().lower()
+        login = record[1].strip()
+        password = record[2].strip()
+        cur.execute(f"INSERT INTO passwords(site_name, login, password) VALUES('{site_name}', '{login}', '{password}');")
         conn.commit()
-        if message.text.strip()[0] == '!':
-            record = message.text[1:].strip().split(',')
-            site_name = record[0].strip().lower()
-            login = record[1].strip()
-            password = record[2].strip()
-            cur.execute(f"INSERT INTO passwords(site_name, login, password) VALUES('{site_name}', '{login}', '{password}');")
-            conn.commit()
-            bot.send_message(message.chat.id, f'Добавлена запись для сайта {site_name}, логин: {login}, пароль: {password}')
-        elif message.text.strip()[0] == '?':
-            request_name = message.text[1:].strip().lower()
-            cur.execute(f"SELECT * FROM passwords WHERE site_name LIKE '%{request_name}%';")
-            results = cur.fetchall()
-            if len(results):
-                result_message = f'По запросу "{request_name}" найдено:\n'
-                for result in results:
-                    print(result)
-                    result_message += f'наименование: {result[1]}, логин: {result[2]}, пароль: {result[3]}\n'
-            else:
-                result_message = f'По запросу "{request_name}" записей не найдено 😔'
-            bot.send_message(message.chat.id, result_message)
-        elif message.text:
-            bot.send_message(message.chat.id, 'Шо ты от меня хочешь, не пойму? Чтобы посмотреть, как правильно '
-                                              'составить запрос, жми на ссылку /help')
+        bot.send_message(message.chat.id, f'Добавлена запись для сайта {site_name}, логин: {login}, пароль: {password}')
+    elif message.text.strip()[0] == '?':
+        request_name = message.text[1:].strip().lower()
+        cur.execute(f"SELECT * FROM passwords WHERE site_name LIKE '%{request_name}%';")
+        results = cur.fetchall()
+        if len(results):
+            result_message = f'По запросу "{request_name}" найдены следующие записи:\n'
+            for result in results:
+                result_message += f'{result[1]}    {result[2]}    {result[3]}\n'
+        else:
+            result_message = f'По запросу "{request_name}" записей не найдено 😔'
+        bot.send_message(message.chat.id, result_message)
+    elif message.text:
+        bot.send_message(message.chat.id, 'Шо ты от меня хочешь, не пойму? Чтобы посмотреть, как правильно '
+                                          'составить запрос, жми на ссылку /help')
 
 
 bot.infinity_polling(interval=0, timeout=20)
